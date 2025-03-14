75c292ba60163c58fbc6d897472d1523fc7b31b2
Igor Nicoletti <igor93nicoletti@gmail.com>
2025-03-14 08:11:33 -0300

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -5,11 +5,11 @@ import { WarningCircle } from "@phosphor-icons/react"
 export const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen px-6 text-center">
-      <WarningCircle size={64} weight="fill" className="text-red-500" />
+      <WarningCircle size={64} weight="fill" className="text-destructive" />
       <h1 className="mt-4 text-4xl font-bold">404</h1>
-      <p className="mt-2 text-lg">Oops! Página não encontrada.</p>
+      <p className="mt-2 text-lg">Oops! Page not found!</p>
       <Button asChild className="mt-6">
-        <Link to="/">Voltar para Home</Link>
+        <Link to="/">Go back to Home</Link>
       </Button>
     </div>
   )
